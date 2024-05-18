cc1d1cbb77c22dcdb7d5be7a876a05b350bf783d
akarshbr <akarshbrajeev@gmail.com>
2024-05-18 19:27:00 +0530

    initial commit

---
+++ smartcampus/smart_app/models.py
@@ -0,0 +1,96 @@
+from django.db import models
+
+# Create your models here.
+from io import BytesIO
+from django.core.files.images import ImageFile
+from django.contrib.auth.models import Group, Permission
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from barcode import generate
+from barcode.writer import ImageWriter
+
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        """
+        Creates and saves a CustomUser with the given username, email, and password.
+        """
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        """
+        Creates and saves a superuser with the given username, email, and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    image = models.ImageField(upload_to='student_images/', blank=True, null=True)
+    admission_number = models.CharField(max_length=10, unique=True)
+    middle_name = models.CharField(max_length=50, blank=True, null=True)
+    date_of_birth = models.DateField(default=None, null=True)
+    blood_group = models.CharField(max_length=3)
+    place = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20)
+    department = models.CharField(max_length=100)
+    course = models.CharField(max_length=100)
+    batch = models.CharField(max_length=10)
+    start_year = models.IntegerField(null=True)
+    end_year = models.IntegerField(null=True)
+    barcode = models.ImageField(upload_to='student_barcodes/', blank=True, null=True)
+    objects=CustomUserManager()
+    def has_perm(self, perm, obj=None):
+        return self.is_staff
+    def has_module_perms(self, app_label):
+        return self.is_staff
+
+    # groups = models.ManyToManyField(Group, related_name='custom_user_groups', blank=True)  # Added blank=True
+    # user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions', blank=True)  # Added blank=True
+
+    def save(self, *args, **kwargs):
+        if not self.pk:  # If the instance is being created
+            if not self.is_superuser:  # Generate barcode only if the user is not a superuser
+                barcode_data = self.admission_number
+                barcode_image = BytesIO()
+                generate('code128', barcode_data, writer=ImageWriter(), output=barcode_image)
+                barcode_image.seek(0)
+                self.barcode.save(f"{self.admission_number}.png", ImageFile(barcode_image, name=f"{self.admission_number}.png"), save=False)
+        
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
+
+
+class Attendance(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE,related_name='user_a')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.admission_number} - {self.timestamp}"
+    
+
+
+class Enqui_student(models.Model):
+    user_name = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name='enquiries')
+    description = models.TextField()
+    enq_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Enquiry by {self.user_name.username} on {self.enq_date}"
---
+++ smartcampus/smart_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('register/',RegisterAPIView.as_view(),name='regg'),
+    path('login/',LoginView.as_view(),name='login'),
+    path('add-attendance/<str:admission_number>/', AddAttendance.as_view(), name='add_attendance'),
+    path('profile/', UserProfile.as_view(), name='user_profile'),
+    path('view-attendance/', ViewAttendance.as_view(), name='view_attendance'),
+    path('enqui-student/', Enqui_studentAPIView.as_view(), name='enqui-student-api'),
+    ]
